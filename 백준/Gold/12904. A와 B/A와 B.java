24de1b18d5c105a1ed4ebf7403a681efd6bfb20c
Hanbh97 <gksqudgus777@naver.com>
2024-10-10 16:22:32 +0900

    [Gold V] Title: A와 B, Time: 112 ms, Memory: 14604 KB -BaekjoonHub

--- 백준/Gold/12904. A와 B/A와 B.java
+++ 백준/Gold/12904. A와 B/A와 B.java
@@ -2,66 +2,39 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class Main{
-	
-	static String start, end;
-	static int result;
-	
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		// 초기 문자
-		start = br.readLine();
+		String start = br.readLine();
 		
 		// 만드려는 문자
-		end = br.readLine();
+		String end = br.readLine();
 		
-		// 시작은 0으로 초기화
-		result = 0;
+		// end를 넣어줌
+		StringBuilder sb = new StringBuilder(end);		
 		
-		make(start, end);
-		System.out.println(result);
-	}
-	
-	// 해당 문자를 만들 수 있는지 찾을 메서드
-	static void make(String start, String end) {
-		StringBuilder sb = new StringBuilder();		
-
-		if(start.length() == end.length()) {			
-			if(start.equals(end)) {
-				result = 1;
-				return;				
-			} else {
-				return;				
+		// 삭제해 나가는 반복문
+		// 초기 문자보다 길때까지 반복
+		while(sb.length() > start.length()) {
+			
+			// 끝 문자가 A면 그냥 제거
+			if(sb.charAt(sb.length() - 1) == 'A') {
+				sb.deleteCharAt(sb.length() - 1);
+			}
+			
+			// 끝 문자가 B면 제거 후 뒤집기
+			else if(sb.charAt(sb.length() - 1) == 'B') {
+				sb.deleteCharAt(sb.length() - 1);
+				sb.reverse();
 			}
 		}
 		
-		// 현재 문자열을 임시로 담을 변수
-		String temp1 = start;
-		
-		// 뒤에 A를 붙이는 경우
-		temp1 += "A";	
-		
-		// 문자열을 뒤집고 B를 추가하는 경우
-		for(int i = start.length() - 1; i >= 0; i--) {
-			sb.append(start.charAt(i));
+		// 작업 후 초기 문자와 같다면 가능함
+		if((sb + "").equals(start)) {
+			System.out.println(1);
+		}else {
+			System.out.println(0);
 		}
-		
-		sb.append("B");
-		
-		// 현재 변경한 모양이 존재하거나, 다음에 변경할 모습이 존재할 경우
-		// 재귀로 다음단계
-		if(end.contains(temp1) || end.contains(sb) || end.contains(temp1) || end.contains(reverse(sb + ""))) {
-			make(temp1, end);			
-			make(sb + "", end);
-		}
-	}
-	
-	static String reverse(String st) {
-		StringBuilder sb = new StringBuilder();	
-		for(int i = st.length() - 1; i >= 0; i--) {
-			sb.append(st.charAt(i));
-		}
-		sb.append("B");
-		return sb + "";
 	}
 }
