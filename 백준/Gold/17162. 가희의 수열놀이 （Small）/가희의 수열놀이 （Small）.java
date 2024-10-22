a5915c4aa9dbb470c78901c280b040d8d256328e
Hanbh97 <gksqudgus777@naver.com>
2024-10-22 09:34:57 +0900

    [Gold IV] Title: 가희의 수열놀이 (Small), Time: 908 ms, Memory: 68128 KB -BaekjoonHub

--- 백준/Gold/17162. 가희의 수열놀이 （Small）/가희의 수열놀이 （Small）.java
+++ 백준/Gold/17162. 가희의 수열놀이 （Small）/가희의 수열놀이 （Small）.java
@@ -6,83 +6,83 @@ import java.util.StringTokenizer;
 public class Main {
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringTokenizer st;
 		StringBuilder sb = new StringBuilder();
 		
-		// 명령어의 개수 Q, 나누는 정수 mod
+		// 명령어의 수 Q, 나누는 정수 mod
+		st = new StringTokenizer(br.readLine());
 		int Q = Integer.parseInt(st.nextToken());
 		int mod = Integer.parseInt(st.nextToken());
 		
-		// mod 만큼의 스택 배열을 선언
-		Stack<Integer>[] stack = new Stack[mod];
-		for(int i = 0; i < mod; i++) {
-			stack[i] = new Stack<>();
+		// 사용할 스택 배열을 선언
+		// 각 나머지 값을 갖는 스택
+		// 해당 스택에 인덱스가 삽입
+		Stack<Integer>[] arr = new Stack[mod];
+		for(int i = 0; i < mod; i++) {			
+			arr[i] = new Stack<>();
 		}
 		
-		// 현재까지 들어간 수들의 개수를 나타낼 변수
-		int cnt = 0;
+		// 들어온 수의 인덱스를 가리킬 변수
+		int idx = 0;
 		
-		// 명령어에 대한 작업을 진행
+		// 반목문으로 명령어를 실행
 		for(int i = 0; i < Q; i++) {
 			st = new StringTokenizer(br.readLine());
 			
-			// 첫 번째 수를 확인
+			// 첫 숫자를 확인
 			int fst = Integer.parseInt(st.nextToken());
 			
-			// 처음 수가 1일 경우 삽입을 진행
+			// 1이면 해당 나머지 인덱스 스택에 삽입
 			if(fst == 1) {
-				int snd = Integer.parseInt(st.nextToken());
-				
-				// 현재 들어간 수 기준 몇 번째 수인지 삽입
-				stack[snd % mod].push(cnt++);
+				int scd = Integer.parseInt(st.nextToken());
+				arr[scd % mod].push(idx++);
 			}
 			
-			// 처음 수가 2일 경우 삭제를 진행
+			// 2면 스택배열을 순회하면서 가장 위의 원소가 가장 최근인 것을 제거
+			// 제거했으므로 idx - 1
 			else if(fst == 2) {
 				
 				for(int j = 0; j < mod; j++) {
 					
-					// 현재 스택이 비지 않았으면서
-					// 바로 이전에 삽입된 숫자이면 그 수를 삭제
-					// 전체 숫자의 개수도 -1
-					if(!stack[j].isEmpty() && stack[j].peek() == cnt - 1) {
-						stack[j].pop();
-						cnt--;
+					if(!arr[j].isEmpty() && arr[j].peek() == idx - 1) {
+						arr[j].pop();
+						idx--;
 						break;
 					}
 				}
 			}
 			
-			// 처음 수가 3일 경우 쿼리를 계산
-			else if(fst == 3) {
-
-				// 가장 작은 수를 담을 변수
+			// 3이면 쿼리 값을 계산
+			else {
+				
+				// 최솟값을 저장할 변수
 				int min = Integer.MAX_VALUE;
 				
-				// 스택배열을 순회
-				for(int j = 0; j < mod; j++) {
+				// 스택배열을 순회하며 가장 위의 원소가 가장 작은 값을 저장
+				// 해당 인덱스까지 뽑아야 한다는 의미이므로 idx - min
+				// 0인 스택이 하나라도 존재한다면 쿼리값은 없음
+				for(int j = 0 ; j < mod; j++) {
 					
-					// 비어있는 스택이 있다면 모든 나머지가 없는 것이므로 -1
-					// 이후 바로 종료
-					if(stack[j].isEmpty()) {
+					if(arr[j].size() == 0) {
 						min = -1;
 						break;
 					}
 					
-					// 최소의 인덱스를 갖는 스택을 찾음
-					if(stack[j].peek() < min) {
-						min = stack[j].peek();
+					
+					if(arr[j].peek() < min) {
+						min = arr[j].peek();
 					}
 				}
 				
+				// min에 -1이 있다면 그대로 min을 출력
+				// 아니면 idx - min을 출력
 				if(min == -1) {
 					sb.append(min).append("\n");
 				}else {
-					sb.append(cnt - min).append("\n");
+					sb.append(idx - min).append("\n");					
 				}
 			}
 		}
-		
 		System.out.println(sb);
 	}
 }
