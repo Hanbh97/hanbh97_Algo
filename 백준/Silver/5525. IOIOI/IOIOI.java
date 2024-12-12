2bf87fd43854584ece8fb4c2fa2c9effaaa95f05
Hanbh97 <gksqudgus777@naver.com>
2024-12-12 18:50:37 +0900

    [Silver I] Title: IOIOI, Time: 244 ms, Memory: 20132 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.java
+++ 백준/Silver/5525. IOIOI/IOIOI.java
@@ -5,40 +5,55 @@ public class Main {
     public static void main(String[] args) throws Exception {
 	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+	// 패턴의 크기
 	int N = Integer.parseInt(br.readLine());
 
-	// 찾을 패턴을 초기화
-	String pt = "I";
-
-	for (int i = 0; i < N; i++) {
-	    pt += "OI";
-	}
-
-	// 패턴 길이
-	int ptL = pt.length();
-
-	// 문자열의 길이와 문자열
+	// 문자열 길이
 	int SL = Integer.parseInt(br.readLine());
+
+	// 문자열
 	String S = br.readLine();
 
-	// 카운팅 변수
+	// 인덱스
+	int idx = 0;
+
+	// 부분 패턴의 개수와 전체 패턴의 개수
+	int pt = 0;
 	int cnt = 0;
 
-	// 패턴 개수 카운팅
-	// 탐색은 SL - ptL 만큼만
-	for (int i = 0; i <= SL - ptL; i++) {
+	// 문자열을 순회하면서 확인
+	while (idx < SL - 2) {
 
-	    // 앞이 I 일때만 탐색
-	    if (S.charAt(i) == 'I') {
+	    // 시작이 I이면
+	    if (S.charAt(idx) == 'I') {
 
-		// 앞부터 일치하는지 확인
-		// 일치하면 두 칸 이동
-		String temp = S.substring(i, i + ptL);
-
-		if (temp.equals(pt)) {
-		    cnt++;
-		    i++;
+		// 이후 O과 I가 연달아 나오면
+		// 부분 패턴 1 증가 후, 인덱스 이동
+		if (S.charAt(idx + 1) == 'O' && S.charAt(idx + 2) == 'I') {
+		    pt++;
+		    idx += 2;
 		}
+
+		// 부분패턴이 없으면 개수 초기화 후 이동
+		else {
+		    pt = 0;
+		    idx++;
+		}
+
+	    }
+
+	    // 시작이 O면 패턴 개수 초기화 후 인덱스 이동
+	    else {
+		pt = 0;
+		idx++;
+	    }
+
+	    // 부분 패턴 개수와 N이 일치하면 패턴 개수 증가
+	    // 다음에 OI가 다시 나올 수 있으므로
+	    // 부분 패턴 개수만 -1
+	    if (pt == N) {
+		cnt++;
+		pt--;
 	    }
 	}
 	System.out.println(cnt);
